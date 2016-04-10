1e25ca4b2efd77e3e134e8af8fa51fb2d70f6370
Steve Lavoie <steven.lavoiejr@gmail.com>
2016-04-09 18:06:19 -1000

    Clean up for clarity

--- python2/gtk2_readtextfile.py
+++ python2/gtk2_readtextfile.py
@@ -12,14 +12,16 @@ import gtk
 import os
 
 class TextFileView:
-    
-    def load_file(self, filename):
+    # setup vars
+    window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+    button = gtk.Button("Load File")
+    textview = gtk.TextView()
+    textbuffer = textview.get_buffer()
+        
+    def read_text_file(self, filename):
         with open(filename) as fn:
             text = fn.read()
         return text
-        
-    def set_text(self, textbuffer, filename):
-        textbuffer.set_text(self.load_file(filename))
         
     def delete_event(self, widget, event, data=None):
         print "delete event occurred"
@@ -30,30 +32,27 @@ class TextFileView:
         gtk.main_quit()
 
     def __init__(self):
-        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL) # create a new window
         self.window.set_default_size(200, 250)
-        # The "delete_event" signal (like File>>Close or X button)
-        # calls the "delete_event" function. The data passed to the callback
-        # function is NULL and is ignored in the callback function.
+        
         self.window.connect("delete_event", self.delete_event)
         self.window.connect("destroy", self.destroy)
-
-        self.textview = gtk.TextView()
-        self.textbuffer = self.textview.get_buffer()      
+        
         self.textview.set_buffer(self.textbuffer)
         
         self.window.set_border_width(10)
-        self.button = gtk.Button("Load File")
+        
         #self.button.connect("clicked", self.set_text, None)
-        self.button.connect_object("clicked", self.set_text, self.textbuffer, os.getcwd() + "/gtk2_base.py")
+        #self.button.connect_object("clicked", self.set_text, self.textbuffer, os.getcwd() + "/gtk2_base.py")
+        self.button.connect_object("clicked", \
+                                    self.textbuffer.set_text, \
+                                    self.read_text_file(os.getcwd() + "/gtk2_base.py"))
+        
         
         # packs widgets into window
         box = gtk.VBox(False, 10)
         self.window.add(box)
         box.pack_start(self.textview)
         box.pack_end(self.button)
-        #self.window.add(self.button)
-        #self.window.add(self.textview)
         self.window.show()
         self.button.show()
         self.textview.show()
