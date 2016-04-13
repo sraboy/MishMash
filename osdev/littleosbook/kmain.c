ecc0367882e57009630c3c79c0bcc36e8920c730
Steven Lavoie <steven.lavoiejr@gmail>
2016-04-13 11:55:48 -1000

    Add kmain.c

---
+++ osdev/littleosbook/kmain.c
@@ -0,0 +1,4 @@
+int kmain(void) {
+	int x = 0xdeadbeef;
+	return x;
+}
--- osdev/littleosbook/loader.asm
+++ osdev/littleosbook/loader.asm
@@ -1,17 +1,32 @@
 global loader                   ; the entry symbol for ELF
 
-MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
+extern kmain
+
+; ************* Constants *************
+KERNEL_STACK_SIZE equ 4096
+MAGIC_NUMBER equ 0x1BADB002     ; multiboot's file signature/magic number
 FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
 								; (magic number + checksum + flags should equal 0)
 
-section .text:                  ; start of the text (code) section
-align 4                         ; the code must be 4 byte aligned
-	dd MAGIC_NUMBER             ; write the magic number to the machine code,
-	dd FLAGS                    ; the flags,
-	dd CHECKSUM                 ; and the checksum
+; ************* BSS *************
+section .bss
+align 4
+; ************* kernel_stack *************
+kernel_stack:					; label will point to the reserved memory
+	resb KERNEL_STACK_SIZE		; NASM-specific pseudo-instruction to reserve memory
 
+; ************* TEXT *************
+section .text:
+align 4                         ; must be 4 byte aligned
+	dd MAGIC_NUMBER             ; writes the constant to the binary,
+	dd FLAGS
+	dd CHECKSUM
+
+; ************* loader *************
 loader:                         ; the loader label (defined as entry point in linker script)
-	mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+	;mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+	mov esp, kernel_stack + KERNEL_STACK_SIZE	;point ESP to top of stack (since it grows downward)
 .loop:
+	call kmain
 	jmp .loop                   ; loop forever
