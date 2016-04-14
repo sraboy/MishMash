c213ab7f05a614d3aa8c529798cf108624300647
Steven Lavoie <steven.lavoiejr@gmail>
2016-04-13 16:21:40 -1000

    Change write_*() to put_char()

--- osdev/littleosbook/kmain.c
+++ osdev/littleosbook/kmain.c
@@ -1,17 +1,22 @@
+/* Framebuffer layout:
+ * Bit:     | 15 14 13 12 11 10 9 8 | 7 6 5 4 | 3 2 1 0 |
+ * Content: | ASCII                 | FG      | BG      |
+*/
+
+#define VGA_BUF 0xb8000;
+
+
+
+
+char * framebuffer = VGA_BUF;
+
 int kmain(void) {
 	int x = 0xdeadbeef;
-	write_32(0x000b8000);
-	write_char(0x000b8000);
+	put_char(0, 'A', 0x8, 0x2);
 	return x;
 }
 
-int write_32(int * dest) {
-	dest[0] = 0x0f41;
-	return 0;
-}
-
-int write_char(char * dest) {
-	dest[2] = 0x42;
-	dest[3] = 0x0f;
-	return 0;
+void put_char(int col, char c, unsigned char fg, unsigned char bg) {
+	framebuffer[col] = c;
+	framebuffer[col + 1] = (fg << 4) | bg;
 }
