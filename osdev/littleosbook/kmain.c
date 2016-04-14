7ef46f3c9f59457ebc09627307e8aba7b1f4076f
Steven Lavoie <steven.lavoiejr@gmail>
2016-04-13 15:57:25 -1000

    Add write functions

--- osdev/littleosbook/kmain.c
+++ osdev/littleosbook/kmain.c
@@ -1,4 +1,17 @@
 int kmain(void) {
 	int x = 0xdeadbeef;
+	write_32(0x000b8000);
+	write_char(0x000b8000);
 	return x;
+}
+
+int write_32(int * dest) {
+	dest[0] = 0x0f41;
+	return 0;
+}
+
+int write_char(char * dest) {
+	dest[2] = 0x42;
+	dest[3] = 0x0f;
+	return 0;
 }
